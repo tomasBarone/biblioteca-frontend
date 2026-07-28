5fe548f84607c85c47cbcfe9719a9b580cc61c5e
Gwynblaidd <tomasBarone@users.noreply.github.com>
2026-07-28 18:44:46 -0300

    feat: Refactor crearOrden function to include headers for email in order creation

--- src/services/ordenService.js
+++ src/services/ordenService.js
@@ -3,8 +3,16 @@ import api from "./api";
 
 export const crearOrden = async (items, email) => {
   try {
-    console.log("Creando orden con items:", items, email);
-    const response = await api.post("/ordenes", items, email );
+    console.log("Creando orden con items:", items, "y email:", email);
+    const response = await api.post("/ordenes", items, {
+      headers: {
+        "Content-Type": "application/json", 
+        "X-User-Email": email,
+      },
+    });
+     
+  
+    console.log("Orden creada:", response.data);
     return response.data;
   } catch (error) {
     console.error("Error al crear la orden:", error);
